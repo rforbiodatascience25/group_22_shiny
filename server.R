e1777070d0109d8bbed60e690678cc58f440df2c
Malte Lau <s224183@student.dtu.dk>
2025-11-04 10:54:57 +0000

    Base_freq function and ui implemented

--- server.R
+++ server.R
@@ -4,6 +4,24 @@ server <- function(input, output) {
     gene_dna(length = input$n_bases, 
              base_probs = c(input$prob_A, input$prob_T, input$prob_C, input$prob_G))
   })
+  output$distPlot <- renderPlot({
+    
+    x <- input$vdna
+    count_A <- lengths(regmatches(x, gregexpr("A", x)))
+    count_T <- lengths(regmatches(x, gregexpr("T", x)))
+    count_C <- lengths(regmatches(x, gregexpr("C", x)))
+    count_G <- lengths(regmatches(x, gregexpr("G", x)))
+    
+    counts <- c(A = count_A, T = count_T, C = count_C, G = count_G)
+    
+    barplot(counts, 
+            col = "#75AADB", 
+            border = "white",
+            xlab = "Nucleotide",
+            ylab = "Count",
+            main = "Nucleotide Counts")
+  })
+
   output$rna <- renderText({
     transcribe_dna(dna = input$vdna)
   })
--- ui.R
+++ ui.R
@@ -78,10 +78,12 @@ ui <- page_fluid(
     card(
       card_header("Virtual Gene Output"),
       mainPanel(
-        verbatimTextOutput(outputId = "dna")
+        verbatimTextOutput(outputId = "dna"),
+        plotOutput(outputId = "distPlot")
       )
     ),
     
+
     # RNA transcription input
     card(
       card_header("Virtual RNA Transcribe"),
